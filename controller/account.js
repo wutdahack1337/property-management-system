1f7e09737b5b4b47cea5d210c151e7ee9c0e6d01
wutdahack1337 <wutdahack1337@gmail.com>
2026-05-24 22:57:45 +0700

    chore: refactor account api

---
+++ controller/account.js
@@ -0,0 +1,43 @@
+import * as service from "../service/account.js";
+
+export async function createAccount(request, response, next) {
+    try {
+        const { residentId, username, password } = request.body;
+        const responseContent = await service.createAccount(residentId, username, password);
+        response.status(201).json(responseContent);
+    } catch (error) {
+        // next(error);
+    }
+}
+
+export async function getAccount(request, response, next) {
+    try {
+        const responseContent = await service.getAccount();
+        response.status(200).json(responseContent);
+    } catch (error) {
+        // next(error);
+    }
+}
+
+
+export async function updateAccount(request, response, next) {
+    try {
+        const id = request.params.id;
+        const { residentId, username, password } = request.body;
+        const responseContent = await service.updateAccount(id, residentId, username, password);
+        response.status(200).json(responseContent);
+    } catch (error) {
+        // next(error);
+    }
+}
+
+
+export async function deleteAccount(request, response, next) {
+    try {
+        const id = request.params.id; 
+        await service.deleteAccount(id);
+        response.status(204).send();
+    } catch (error) {
+        // next(error);
+    }
+}
